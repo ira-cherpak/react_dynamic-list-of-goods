89d1b6452062a41703316c070c0a80245aab9028
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-01-26 00:30:12 +0200

    Updates

---
+++ precache-manifest.5a4d9b205ddf9786c7dbb78ac8cf7cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff141701080707478be50e21f024ffe5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24eab366cb9133e0f500",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "bdc304a3f98d513a5043",
+    "url": "./static/js/2.a7c1b048.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a7c1b048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24eab366cb9133e0f500",
+    "url": "./static/js/main.ed012ed6.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
